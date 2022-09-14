f43da181c3f1b0357c1ad4886be1a182b60a26bc
jaycee-li <jayceeli@google.com>
2022-09-13 22:31:35 -0700

    Update autolog.py

--- google/cloud/aiplatform/metadata/autolog.py
+++ google/cloud/aiplatform/metadata/autolog.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import aiplatform
+from google.cloud import aiplatform
 
 
 class RunInfo:
